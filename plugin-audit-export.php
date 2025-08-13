f87c17764f87d944c8ebf38dafbffbc1c8fcb088
Kaitlyn Hennessy <kaitlyn@codingit.dev>
2025-08-13 10:32:46 -0400

    remove unneeded consts

--- plugin-audit-export.php
+++ plugin-audit-export.php
@@ -14,11 +14,6 @@ namespace PluginAuditExporter;
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
-// Useful constants.
-define( 'PLUGIN_AUDIT_EXPORT_VERSION', '0.1.0' );
-define( 'PLUGIN_AUDIT_EXPORT_FILE', __FILE__ );
-define( 'PLUGIN_AUDIT_EXPORT_DIR', plugin_dir_path( __FILE__ ) );
-
 require_once(__DIR__ . '/classes/PluginAuditExporterAdmin.php');
 add_action('plugins_loaded', function() {
     if (is_admin()) {
